814e7c2bf939bf38a64b0746c4d2025becf80c95
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-20 13:12:24 +0100

    implement form and list components to allow a user input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { useState } from 'react';
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+  { id: 3, description: 'Charger', quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <Logo />
       <Form />
       <PackingList />
@@ -12,18 +20,69 @@ function Logo() {
   return <h1>🌴 Far Away 🧳</h1>;
 }
 function Form() {
+  const [userInput, setUserInput] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userInput, 'here');
+    setUserInput('');
+  };
+
   return (
-    <div class="add-form">
+    <form
+      className="add-form"
+      onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select
+        name=""
+        id="">
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option
+            value={num}
+            key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
-  return <div class="list">List</div>;
+  return (
+    <div className="list">
+      <ul className="list">
+        {initialItems.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li key={item.id}>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+        <button>❌</button>
+      </span>
+    </li>
+  );
+}
+
 function Stats() {
   return (
-    <footer class="stats">
+    <footer className="stats">
       <em>💼 You have x items on your list, and you already packed X (X%)</em>
     </footer>
   );
