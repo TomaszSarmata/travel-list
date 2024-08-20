b2ddb9135899a5bcd2a437bbf45853688066a377
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-20 11:17:35 +0100

    created layout of the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-function App() {
-  return <div className="App">Hello</div>;
+export default function App() {
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
-
-export default App;
+function Logo() {
+  return <h1>🌴 Far Away 🧳</h1>;
+}
+function Form() {
+  return (
+    <div class="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div class="list">List</div>;
+}
+function Stats() {
+  return (
+    <footer class="stats">
+      <em>💼 You have x items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
